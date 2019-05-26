f815af427d7e4634cd1957e179c0ff19d9c9886d
fanghui cheng <fanghuicheng@fanghuideMacBook-Pro.local>
2019-05-26 17:40:48 +0800

    init test-pro

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by fanghuicheng on 2019-05-26 17:37
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("hello world! i am java");
+    }
+
+}
---
+++ src/main/scala/ScalaTest.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by fanghuicheng on 2019-05-26 17:38
+  */
+object ScalaTest {
+
+  def main(args: Array[String]): Unit = {
+    println("hello world ! i am scala")
+  }
+
+}

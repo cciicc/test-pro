9a086fafb891b5620cd3531511d14425cc6d2ea3
fanghui cheng <fanghuicheng@fanghuideMacBook-Pro.local>
2019-05-27 22:53:22 +0800

    branch2 first change

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,7 @@ public class Test {
 
     public static void main(String[] args){
         System.out.println("hello world! i am java");
+        System.out.println("hello world! i am test branch2 change1");
     }
 
 }
--- src/main/scala/ScalaTest.scala
+++ src/main/scala/ScalaTest.scala
@@ -5,6 +5,7 @@ object ScalaTest {
 
   def main(args: Array[String]): Unit = {
     println("hello world ! i am scala")
+    println("hello world ! i am test branch2 change1")
   }
 
 }
